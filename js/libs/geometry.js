5f5a511f294bf5448c36a82e87306b004b7653a8
ShadowIterator <ShadowIterator@hotmail.com>
2018-07-15 21:22:27 +0800

    mainframe

---
+++ js/libs/geometry.js
@@ -0,0 +1,168 @@
+export const EPS = 0.00000001;
+export const PI = Math.PI;
+export const INF = 99888000;
+
+export function DBcmp(a, b) {
+    if(a - b > EPS) return 1;
+    if(b - a > EPS) return -1;
+    return 0;
+}
+
+export class Point {
+    constructor(tx, ty) {
+        this.x = tx;
+        this.y = ty;
+    }
+}
+
+export class Circle {
+    constructor(tO, tR) {
+        this.O = tO;
+        this.R = tR;
+    }
+}
+
+export class Segment {
+    constructor(tP1, tP2) {
+        this.P1 = tP1;
+        this.P2 = tP2;
+    }
+}
+
+export class Colli {
+    constructor(tCo) {
+        this.P = tCo.P;
+        this.N = tCo.N;
+        this.d = tCo.d;
+    }
+}
+
+export function add(V1, V2) {
+    return new Point(V1.x + V2.x, V1.y + V2.y);
+}
+
+export function _add(V1, V2) {
+    V1.x += V2.x;
+    V1.y += V2.y;
+}
+
+export function sub(V1, V2) {
+    return new Point(V1.x - V2.x, V1.y - V2.y);
+}
+
+export function _sub(V1, V2) {
+    V1.x -= V2.x;
+    V1.y -= V2.y;
+}
+
+export function mul(V, k) {
+    return new Point(V.x * k, V.y * k);
+}
+
+export function _mul(V, k){
+    V.x *= k;
+    V.y *= k;
+}
+
+export function rotate(V, theta) {
+    let cosT = Math.cos(theta);
+    let sinT = Math.sin(theta);
+    return new Point(V.x * cosT - V.y * sinT,
+        V.x * sinT + V.y * cosT);
+}
+
+export function _rotate(V, theta) {
+    let cosT = Math.cos(theta);
+    let sinT = Math.sin(theta);
+    let x = V.x;
+    let y = V.y;
+
+    V.x = x * cosT - y * sinT;
+    V.y = x * sinT + y * cosT;
+}
+
+export function dot(V1, V2) {
+    return V1.x * V2.x + V1.y * V2.y;
+}
+
+export function cross(V1, V2) {
+    return V1.x * V2.y - V2.x * V1.y;
+}
+
+export function _reverse(V){
+    V.x = -V.x;
+    V.y = -V.y;
+}
+
+export function mod_2(V) {
+    return V.x * V.x + V.y * V.y;
+}
+
+export function mod(V) {
+    return Math.sqrt(mod_2(V));
+}
+
+export function _normalize(V) {
+    let len = mod(V);
+    if(DBcmp(len, 0) === 0) return ;
+    V.x = V.x / len;
+    V.y = V.y / len;
+}
+
+export function normalize(V) {
+    let len = mod(V);
+    if(DBcmp(len, 0) === 0) return new Point(0,0);
+    return new Point(V.x / len, V.y / len);
+}
+
+export function _relen(V, k) {
+    let len = mod(V);
+    if(DBcmp(len, 0) === 0) return ;
+    V.x = V.x / len * k;
+    V.y = V.y / len * k;
+}
+
+export function dist_2(P1, P2) {
+    let dx = P1.x - P2.x;
+    let dy = P1.y - P2.y;
+    return dx * dx + dy * dy;
+}
+
+export function relen(V, k) {
+    let len = mod(V);
+    if(DBcmp(len, 0) === 0) return new Point(0,0);
+    return new Point(V.x / len * k, V.y / len * k);
+}
+
+export function isZero(P) {
+    return !!(DBcmp(P.x, 0) === 0, DBcmp(P.y, 0) === 0);
+}
+
+export function _toZero(V) {
+    V.x = V.y = 0;
+}
+
+export function calH(L, O) {
+    let V1 = sub(L.P1, L.P2);
+    _normalize(V1);
+    let V2 = sub(O, L.P1);
+    let dPP = dot(V1, V2);
+    _mul(V1, dPP);
+    return add(L.P1, V1);
+}
+
+export function pointOnSegment(P, S) {
+    let V1 = sub(S.P1, P);
+    let V2 = sub(S.P2, P);
+    return DBcmp(dot(V1, V2), 0) < 0;
+}
+
+
+
+export function pointInCircle(P, O) {
+    return DBcmp(dist_2(P, O.O), O.R * O.R) <= 0;
+}
+
+export function CircleOnCircle(P, C, R) {
+    return DBcmp(dist_2(P, C.O), (C.R + R) * (C.R + R)) < 0;
+}
---
+++ js/main.js
@@ -0,0 +1,63 @@
+// import Player from './player/index'
+// import Enemy from './npc/enemy'
+// import BackGround from './runtime/background'
+// import GameInfo from './runtime/gameinfo'
+// import Music from './runtime/music'
+// import DataBus from './databus'
+
+// import Stone from './stone/stone'
+
+let ctx = canvas.getContext('2d')
+// let databus = new DataBus()
+
+/**
+ * 游戏主函数
+ */
+let CL = [];
+let LL = [];
+export default class Main {
+    constructor() {
+        // 维护当前requestAnimationFrame的id
+        this.aniId = 0;
+        // this.stone = new Stone(canvas.width, canvas.height,
+        //     canvas.width/2 , canvas.height - 50,
+        //     1, 1.5,
+        //     0.6, 0.1, 15);
+        // console.log(canvas.height - 40);
+        this.restart();
+    }
+
+    restart() {
+        canvas.removeEventListener(
+            'touchstart',
+            this.touchHandler
+        );
+
+        this.bindLoop = this.loop.bind(this);
+
+        window.cancelAnimationFrame(this.aniId);
+
+        this.aniId = window.requestAnimationFrame(
+            this.bindLoop,
+            canvas
+        );
+    }
+
+    render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // this.stone.render(ctx);
+
+    }
+    // // 实现游戏帧循环
+    loop() {
+        // databus.frame++
+        this.frame++;
+        // console.log(this.stone.O.x, this.stone.O.y);
+        // this.render();
+        // this.stone.update();
+        this.aniId = window.requestAnimationFrame(
+            this.bindLoop,
+            canvas
+        )
+    }
+}
