d8d945950da518cdb1a523d60c88491e6cb6bd7a
sherlockcooper <sherlockcooper@sina.com>
2018-07-21 18:55:32 +0800

    enemy image added

--- js/eles/enemy.js
+++ js/eles/enemy.js
@@ -9,6 +9,7 @@ import {DBcmp, add, _add, sub, _sub, mul, _mul,
     pointOnSegment, pointInCircle,
     CircleOnCircle} from '../libs/geometry'
 
+const enemySrc='images/enemy.png';
 
 const IMGSRC = '';
 const ENEMY_WIDTH = 10;
@@ -26,6 +27,8 @@ export default class Enemy extends Sprite{
         this.routine = [];
         this.maxy = this.shape.O.y;
         this.className = 'enemy';
+
+        this.setImg(enemySrc);
     }
 
     updateV() {
@@ -52,11 +55,12 @@ export default class Enemy extends Sprite{
 
     drawToCanvas(ctx, transPosition) {
         let P = transPosition(this.shape.O);
-        ctx.beginPath();
-        ctx.arc(P.x, P.y, this.shape.R, 0, 2 * PI, false);
-        ctx.strokeStyle = '#f00';
-        ctx.lineWidth = 2;
-        ctx.stroke();
+        // ctx.beginPath();
+        // ctx.arc(P.x, P.y, this.shape.R, 0, 2 * PI, false);
+        // ctx.strokeStyle = '#f00';
+        // ctx.lineWidth = 2;
+        // ctx.stroke();
+        ctx.drawImage(this.img, P.x-this.shape.R, P.y-this.shape.R, this.width, this.height);
     }
 
 
